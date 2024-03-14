003c81fac2f50dc4664152de63a122d333d8436a
Guido Stocco <guidostoccodev@gmail.com>
2024-03-14 20:51:47 -0300

    Operadores lógicos e aritméticas

---
+++ 02_Operadores/js/script.js
@@ -0,0 +1,64 @@
+/* // 1 - Number
+console.log(typeof 2);
+console.log(typeof 57.3);
+console.log(typeof -932);
+
+// 2 - Operações Aritméticas
+
+console.log(10 * 10);
+console.log(10/2);
+
+// Special Numbers
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(12 * "guido");
+
+// Strings
+
+console.log("Guido");
+console.log(`Guido`);
+console.log('Guido');
+
+// Mais sobre string
+
+console.log("Quebra de \nlinha");
+
+// Concatenação
+console.log("Oi, " + "Tudo " + "Bem?")
+
+// Interpolação, Template Strings
+
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
+
+// Booleans
+console.log(true);
+console.log(5 < 20);
+console.log(2 > 10);
+
+// Comparações
+console.log(5 <= 5);
+console.log( 5 >= 3);
+console.log( 5 <= 3);
+console.log( 10 == 10);
+console.log(10 != 9);
+
+// Idêntico
+console.log(9 == "9");
+console.log(9 === "9");
+console.log(9 != "9");
+console.log(9 !== "9"); //tipo é diferente */
+
+// Operadores Lógicos
+
+console.log(true && true);
+console.log(true && false);
+console.log(true || false);
+console.log(false || false);
+
+// Empty Values
+console.log(typeof null, typeof undefined);
+console.log(null === undefined);
+console.log(null == undefined);
+
