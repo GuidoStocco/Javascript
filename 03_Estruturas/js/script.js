893b891d959c8fb69c0a4ab51529d6f3f824d42f
Guido Stocco <guidostoccodev@gmail.com>
2024-03-15 20:51:10 -0300

    Update script.js

---
+++ 03_Estruturas/js/script.js
@@ -0,0 +1,23 @@
+// Variáveis
+
+let nome = "Guido";
+console.log(nome);
+
+nome = "Guido Stocco";
+console.log(nome);
+
+const age = 31;
+console.log(age);
+
+// Mais sobre variáveis
+
+let a = 2, 
+b = 3, 
+c = 4;
+
+console.log(a, b, c);
+
+let _test = "ok";
+let $test = "ok";
+console.log (_test);
+console.log($test);
