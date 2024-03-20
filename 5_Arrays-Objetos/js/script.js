cf42b811c29b5fab8b108f0bbc9dbf829ad3fcf9
Guido Stocco <guidostoccodev@gmail.com>
2024-03-19 22:48:36 -0300

    Update script.js

--- 5_Arrays-Objetos/js/script.js
+++ 5_Arrays-Objetos/js/script.js
@@ -2,3 +2,28 @@
 const lista = [1, 2, 3, 4, 5]
 console.log(lista);
 console.log(typeof lista);
+
+// PROPRIEDADES
+
+const number = [5, 3, 4];
+
+console.log(number.length);
+
+console.log(number["length"]);
+
+const myName = "Guido";
+
+console.log(myName.length);
+console.log(myName["length"]);
+
+// MÉTODOS
+
+const otherNumbers = [1, 2, 3];
+
+const allNumbers = number.concat(otherNumbers);
+console.log(allNumbers);
+//
+
+const txt = "guido";
+
+console.log(txt.toUpperCase());
