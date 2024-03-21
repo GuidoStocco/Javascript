e59aca7608f10b1b786c129e833757b3c8186679
Guido Stocco <guidostoccodev@gmail.com>
2024-03-20 22:06:22 -0300

    Update script.js

--- 5_Arrays-Objetos/js/script.js
+++ 5_Arrays-Objetos/js/script.js
@@ -1,4 +1,4 @@
-// ARRAYS
+/*// ARRAYS
 const lista = [1, 2, 3, 4, 5]
 console.log(lista);
 console.log(typeof lista);
@@ -24,6 +24,83 @@ const allNumbers = number.concat(otherNumbers);
 console.log(allNumbers);
 //
 
-const txt = "guido";
+const txt = "GUIDO";
 
-console.log(txt.toUpperCase());
+console.log(txt.toLowerCase());
+
+// OBJETOS 
+
+const person = {
+    name: "Guido",
+    age: 24,
+    job: "Programador",
+};
+console.log(person);
+console.log(person.name);
+
+// MAIS SOBRE OBJETOS
+
+const obj = {
+    a: "teste",
+    b: true,
+};
+console.log(obj);
+//
+const obj2 = {
+    c: ["Guido"],
+};
+Object.assign(obj2, obj);
+console.log(obj2);
+
+// CONHECENDO MELHOR OS OBJETOS
+
+console.log(Object.keys(obj));
+console.log(Object.keys(obj2));
+console.log(Object.entries(obj));
+
+// MUTAÇÃO
+
+const a = {
+    name: "Guido",
+};
+const b = a;
+console.log(a);
+console.log(b);
+console.log(a === b);
+
+a.age = 31;
+console.log(b); 
+*/
+
+// LOOP EM ARRAY
+
+const users = ["guido", "mateus", "joão", "miguel"];
+
+for(let i = 0; i < users.length; i++){
+    console.log(`Listando o usuário: ${users[i]}`);
+}
+
+// PUSH AND POP
+
+const array = ['a', 'b', 'c'];
+array.push('d');
+console.log(array);
+
+array.pop();
+console.log(array);
+
+// SHIFT UNSHIFT
+
+const letters = ["a", "b", "c"];
+const letter = letters.shift();
+console.log(letters);
+
+letters.unshift("z", "s");
+console.log(letters);
+
+
+// indexOf e lastIndexOf
+
+const myElements = ['morango', 'pêra', 'maça', 'abacate', 'uva'];
+console.log(myElements.indexOf('pêra'));
+console.log(myElements.lastIndexOf('uva'));
