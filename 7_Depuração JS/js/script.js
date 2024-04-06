99422ff5482122c75a3225cf57f198c508687f83
Guido Stocco <guidostoccodev@gmail.com>
2024-04-05 21:12:44 -0300

    Método debugger

--- 7_Depuração JS/js/script.js
+++ 7_Depuração JS/js/script.js
@@ -1,3 +1,44 @@
 // STRICT MODE
 "use strict";
 
+
+// CONSOLE.LOG
+
+let a = 1
+let b = 2
+
+if( a == 1){
+    a = b + 2
+}
+
+console.log(a);
+
+for(let i = 0; i < b; i++){
+    a = a + 2
+    console.log(a);
+};
+
+
+// DEBUGGER
+
+let c = 1
+let d = 2
+
+if(c == 1){
+    c = d + 2
+}
+//debugger;
+
+for(let i = 0; i < d; i++){
+    c = c + 2;
+};
+
+console.log('executou o loop');
+//debugger;
+
+if(c > 5){
+    c = 25
+}
+
+
+//
