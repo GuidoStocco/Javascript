d53e04e1270bab40e13b1eaf747a456e8a07fa92
Guido Stocco <guidostoccodev@gmail.com>
2024-04-07 20:27:42 -0300

    Função setInterval

--- 8_Assíncrona/js/script.js
+++ 8_Assíncrona/js/script.js
@@ -7,4 +7,15 @@ setTimeout(function(){
 console.log("Ainda não executou");
 
 
+// FUNÇÃO setInterval
+
+console.log("Ainda não executou 2");
+
+//setTimeout(function(){
+//    console.log("Intervalo Assíncrono");
+//}, 3000);
+
+console.log("Ainda não executou 2");
+
+
 //
