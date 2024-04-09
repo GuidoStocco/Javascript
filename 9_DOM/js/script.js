b386a1eac7c68020950478aced203e1f2ee3d614
Guido Stocco <guidostoccodev@gmail.com>
2024-04-09 20:27:20 -0300

    Selecionando elemento por ID

--- 9_DOM/js/script.js
+++ 9_DOM/js/script.js
@@ -1,4 +1,16 @@
 // MOVENDO-SE PELO DOM
+/*
 console.log(document.body);
 console.log(document.body.childNodes);
 console.log(document.body.childNodes[1]);
+*/
+
+// Selecionando por Tags getElementsByTagName
+
+const listItems = document.getElementsByTagName("li")
+console.log(listItems);
+
+
+// Selecionando o elemento pelo ID
+const title = document.getElementById("tittle");
+console.log(title);
