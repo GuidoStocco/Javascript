def17a653b896c5cadb66643ff0e0439c4a66705
Guido Stocco <guidostoccodev@gmail.com>
2024-04-16 20:05:11 -0300

    reduce, find, findIndex e some

--- 11_Array-Iteradores/js/script.js
+++ 11_Array-Iteradores/js/script.js
@@ -13,6 +13,23 @@ const preco = [3.50, 5, 3.50, 2, 2.50, 0]
 //});
 
 // reduce
-const total = preco.reduce((acc, valor, index, array) =>{
-    return acc + valor
-}, 0);;
+//const total = preco.reduce((acc, valor, index, array) =>{
+//    return acc + valor
+//}, 0);;
+
+
+// find - retorna todo valor q for true
+//const resultado = estoque.find((valor, index, array) => {
+  //  return  valor;
+//});
+
+
+// findindex - retorna o index desse valor
+//const result = estoque.findIndex((valor, index, array) => {
+ //  return  valor;
+//});
+
+
+// some - vai retorna um boolean, true ou false. Precisa ter um item para retorna true
+const resultado1 = estoque.some((valor, index, array) => valor);
+
