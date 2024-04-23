96b03e457ed3c03acf56987311589fab14aa3c1f
Guido Stocco <guidostoccodev@gmail.com>
2024-04-23 19:36:41 -0300

    Update script.js

--- 1_exercicios/js/script.js
+++ 1_exercicios/js/script.js
@@ -5,7 +5,7 @@ let gs = true;
 
 if(name == "joao" && age == 24){
     console.log("nome é " + name)
-} else if(name == " "){
+} else if(name == "Guido"){
     console.log(name)
 } else{
     console.log("nenhuma das alternativas")
