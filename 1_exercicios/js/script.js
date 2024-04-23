2ca6a027c5c9ff3f7c073a3304b687e9587c092e
Guido Stocco <guidostoccodev@gmail.com>
2024-04-23 20:21:11 -0300

    upadate
    armazenando valores, atividade

--- 1_exercicios/js/script.js
+++ 1_exercicios/js/script.js
@@ -15,3 +15,27 @@ let idade = 18
 if(idade >= 18){
     console.log("Você pode entrar na balada")
 };
+
+
+// criando uma constante
+const nome = "Guido";
+
+if(nome == "Guido"){
+    console.log(`Olá ${nome}, boas vindas`)
+};
+
+// Expoente
+const numb = 24;
+
+console.log(Math.pow(3,2));
+console.log(Math.pow(5,5));
+console.log(Math.pow(numb,2));
+
+//armazenando valor
+let velo = 90;
+
+if(velo > 80){
+    console.log("Você está no limite permitido")
+} else{
+    console.log("Você está acima do limite permitido")
+};
