ae330d6683803e9180d5df72b00af4274e7c59c2
Guido Stocco <guidostoccodev@gmail.com>
2024-04-23 19:36:16 -0300

    if,else, else if

---
+++ 1_exercicios/js/script.js
@@ -0,0 +1,13 @@
+// ARMAZENANDO VALORES STRING, NUMBER, BOOLEAN
+let name = "Guido";
+let age = 24;
+let gs = true;
+
+if(name == "joao" && age == 24){
+    console.log("nome é " + name)
+} else if(name == " "){
+    console.log(name)
+} else{
+    console.log("nenhuma das alternativas")
+};
+
