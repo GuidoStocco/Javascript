9a6aefdff8df0b9a2bcecfbef2126da0007ce2ca
Guido Stocco <guidostoccodev@gmail.com>
2024-04-27 18:37:46 -0300

    upgrade
    loop em arrays

--- 12_Objetos/js/script.js
+++ 12_Objetos/js/script.js
@@ -62,4 +62,12 @@ const muta = {
 console.log(mut == mutacao);
 console.log(mut == muta);
 
-//
+// loop em arrays
+
+let numeros = [1,5,10,15,20,25];
+
+for(let i = 0; i <= numeros.length; i++){
+    console.log(numeros[i]);
+}
+
+
