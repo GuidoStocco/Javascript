1a833948e5aa36b4c9ddc2a0c29a12f5b8eb29f9
Guido Stocco <guidostoccodev@gmail.com>
2024-05-02 21:53:49 -0300

    Update script.js

--- 13_OObjetos/js/script.js
+++ 13_OObjetos/js/script.js
@@ -1,3 +1,17 @@
 // ORIENTAÇÃO A OBJETOS
 
 // MÉTODOS
+const gato = {
+    miado: function() {
+        console.log("Miauu")
+    },
+    pulo: function(){
+        console.log("Pulou")
+    }
+};
+
+gato.miado();
+gato.pulo();
+
+
+// MAIS SOBRE MÉTODOS
