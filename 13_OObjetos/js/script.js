17d724f325fd6e006795e2c87e13611ba71ead43
Guido Stocco <guidostoccodev@gmail.com>
2024-05-06 19:21:33 -0300

    orientação a objetos

--- 13_OObjetos/js/script.js
+++ 13_OObjetos/js/script.js
@@ -15,3 +15,35 @@ gato.pulo();
 
 
 // MAIS SOBRE MÉTODOS
+const cachorro = {
+    raca: "SRD",
+    latir: function() {
+        console.log("au au")
+    },
+    pulo: function(){
+        console.log("Pulou")
+    },
+    setRaca: function(raca){
+        this.raca = raca;
+    },
+    getRaca: function(){
+        return "A raça é " + this.raca;
+    }
+};
+
+console.log(cachorro.raca);
+
+cachorro.setRaca("Pastor Alemão");
+
+console.log(cachorro.raca);
+
+console.log(cachorro.getRaca())
+
+
+
+// PROTOTYPE
+const pessoa = {
+    pe: 2,
+};
+
+console.log(Object.getPrototypeOf(pessoa));
