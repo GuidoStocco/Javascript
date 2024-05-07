852253a95b5d4b18e6e52cb95925a5445e1c5232
Guido Stocco <guidostoccodev@gmail.com>
2024-05-06 21:46:19 -0300

    Update script.js

--- 13_OObjetos/js/script.js
+++ 13_OObjetos/js/script.js
@@ -47,3 +47,52 @@ const pessoa = {
 };
 
 console.log(Object.getPrototypeOf(pessoa));
+
+
+// MAIS SOBRE PROTOTYPE
+let carro = {
+    porta: 2,
+};
+
+let carroNovo = Object.create(carro);
+
+console.log(carroNovo.porta);
+console.log(Object.getPrototypeOf(carroNovo) === carro);
+
+
+
+// CLASSES
+let dog = {
+    patas: 4,
+    raca: "SRD",
+    latir: function (){
+        console.log("au au")
+    }
+};
+
+let labrador = Object.create(dog);
+
+labrador.latir();
+labrador.raca = "labrador";
+
+console.log(labrador.raca);
+console.log(dog.raca);
+
+let pastor = Object.create(dog); // Outro objecto a partir da classe que aproveita as propriedades do prototype
+pastor.raca = "Pastor Alemão";
+
+console.log(pastor.raca);
+
+
+
+//Instanciando classes por função
+function criarCachorro(raca, patas, cor){
+    let cachorro = Object.create({});
+    cachorro.raca = raca;
+    cachorro.patas = patas;
+    cachorro.cor = cor;
+    return cachorro;
+};
+let pastorAlemao = criarCachorro("Pastor Alemão", 4, "Marrom");
+console.log(pastorAlemao);
+console.log(pastorAlemao.cor);
