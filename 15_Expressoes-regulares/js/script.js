452aa6967bf8eb90b0a1a1de30ce74df29f5c041
Guido Stocco <guidostoccodev@gmail.com>
2024-05-14 20:58:55 -0300

    Update script.js

--- 15_Expressoes-regulares/js/script.js
+++ 15_Expressoes-regulares/js/script.js
@@ -1,3 +1,22 @@
 //Expressoes regulares
 // New RegExp ou /.../
 
+const reg1 = new RegExp('marinho');
+
+console.log(reg1.test('Guido marinho stocco'));
+console.log(reg1.test("não tem o marinho"));
+
+let text = "meu nome é guido marinho stocco";
+
+console.log(reg1.test(text));
+
+
+//conjunto utilizando o []
+const reg2 = /12345/;
+
+console.log(reg2.test('o conjunto tem o número 6?'));
+console.log(reg2.test('o conjunto tem o número 2?'));
+
+const reg3 = /0-9/; // jeito certo de fazer
+
+console.log(reg3.test('temos o numero 9'))
