7118b0192aa2b87697f42172443e3c3428b380f5
Guido Stocco <guidostoccodev@gmail.com>
2024-05-17 20:50:03 -0300

    creat DOM

--- 16_Assíncrona2/js/script.js
+++ 16_Assíncrona2/js/script.js
@@ -94,4 +94,22 @@ async function multiply(b,c,d) {
     return await x * y;
 };
 
-multiply(5,3,2).then((value) => console.log(value));
+multiply(8,2,6).then((value) => console.log(value));
+
+
+
+// generators
+function* criadorId(){
+    let id = 0;
+    while(true){
+        yield id++
+    };
+};
+
+let criarId = criadorId();
+
+console.log(criarId.next().value);
+console.log(criarId.next().value);
+console.log(criarId.next().value);
+
+
