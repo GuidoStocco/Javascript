7ef0efe05845552e08269b7e904228bc1698927f
Guido Stocco <guidostoccodev@gmail.com>
2024-05-20 21:03:09 -0300

    Update script.js

--- 11_Array-Iteradores/js/script.js
+++ 11_Array-Iteradores/js/script.js
@@ -2,10 +2,10 @@
 const estoque = ['arroz', 'feijao', 'macarrão', 'açucar', 'batata', '']
 const preco = [3.50, 5, 3.50, 2, 2.50, 0]
 
-// forEach
-//estoque.forEach((valor, index, array ) => {
-  //  console.log(valor, index)
-//}); 
+ //forEach
+estoque.forEach((valor, index, array ) => {
+    console.log(valor, index)
+}); 
 
 // map
 //const retornoMap = estoque.map((valor, index, array) => {
