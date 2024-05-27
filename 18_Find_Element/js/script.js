18c0a4ef43d8894bc71db3dceddddad13d304139
Guido Stocco <guidostoccodev@gmail.com>
2024-05-27 20:55:36 -0300

    upgrade

--- 18_Find_Element/js/script.js
+++ 18_Find_Element/js/script.js
@@ -2,3 +2,11 @@
 console.log(document.getElementsByTagName('h1'));
 
 // Encontrando elementos por Id
+console.log(document.getElementById('#byid'))
+
+//Encontrando elemento por Classname
+console.log(document.getElementsByClassName('.paragrafo'));
+
+// Encontrando elementos por querySelector
+
+console.log(document.querySelector('h2'));
