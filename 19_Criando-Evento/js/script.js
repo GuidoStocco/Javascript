4e3293868c4a6a59043566be3bf9e553fb52e350
Guido Stocco <guidostoccodev@gmail.com>
2024-05-28 20:54:38 -0300

    Eventos

--- 19_Criando-Evento/js/script.js
+++ 19_Criando-Evento/js/script.js
@@ -4,10 +4,36 @@
 
 let btn1 = document.querySelector('.button-1');
 
-btn1.addEventListener("click", () => {
-    console.log('Clicou em mim');
-});
+function msg(){
+    console.log("clicou aqui")
+};
+
+btn1.addEventListener("click", msg);
 
 
 // Removendo o evento
 //removeEventListener
+
+let btn2 = document.querySelector(".button-2");
+
+btn2.addEventListener("click", () => {
+    btn1.removeEventListener("click", msg)
+});
+
+
+
+// Objeto do Evento
+function msg(e){
+    console.log(e)
+};
+
+
+// Evento por tecla (key event)
+// keyup e keydown
+window.addEventListener("keydown", (e) => {
+    
+    if(e.key == 's'){
+        console.log("Apertou a tecla S")
+    };
+
+});
