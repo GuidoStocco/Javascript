4fd13d763ba54a00acda6422972bed471e3a89a8
Guido Stocco <guidostoccodev@gmail.com>
2024-05-28 19:48:35 -0300

    event listener
    adicionando evento

--- 19_Criando-Evento/js/script.js
+++ 19_Criando-Evento/js/script.js
@@ -1,3 +1,13 @@
 // Criando Evento
 
 // addEventListener
+
+let btn1 = document.querySelector('.button-1');
+
+btn1.addEventListener("click", () => {
+    console.log('Clicou em mim');
+});
+
+
+// Removendo o evento
+//removeEventListener
