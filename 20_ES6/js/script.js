bfb27d16c555a092a5e8c3212f4e4650f24a06fe
Guido Stocco <guidostoccodev@gmail.com>
2024-05-28 22:45:51 -0300

    Update script.js

--- 20_ES6/js/script.js
+++ 20_ES6/js/script.js
@@ -70,3 +70,59 @@ const {nome, idade, profissao: dev} = pessoa
 
 console.log(dev)
 console.log(idade)
+
+
+
+// Spread Operator
+// constroi novos valores destes dados em outros array e objetos
+const num = [1,2,3];
+const num1 = [4,5,6];
+
+const numero = [...num,...num1]
+
+console.log(numero);
+//Adiciona elementos no meio de outros elementos
+
+const a1 = [1000, ...num, 9999]
+console.log(a1)
+
+
+
+// Class
+// fundamental para programar em orientação a objetos
+class Products {
+    constructor (nome, price){
+        this.nome = nome;
+        this.price = price;
+    };
+
+    productComDesconto(discount){
+        return this.price * ((100 - discount)/ 100)
+    };
+};
+
+const shirt = new Products("Camisa", 20);
+console.log(shirt.nome)
+console.log(shirt)
+
+
+
+// Herança
+//utilizamos o extends e o super para os parâmetros
+class ColorProducts extends Products {
+    constructor(nome, price, color){
+        super(nome,price)
+        this.color = color;
+    };
+
+    showColors(){
+        console.log("As cores são:")
+        this.color.forEach((colors) =>{
+            console.log(colors)
+        })
+    }
+};
+
+const cores = new ColorProducts('guido', 24.99, ['vermelho', 'azul', 'crinza'])
+
+cores.showColors();
