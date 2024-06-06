6eaec88f3323835cb7e15558dfc3f02c1e6a88b9
Guido Stocco <guidostoccodev@gmail.com>
2024-06-06 18:34:57 -0300

    treinando estrutura de controle

--- 21_Lógica_Algoritmo/js/script.js
+++ 21_Lógica_Algoritmo/js/script.js
@@ -1,1 +1,9 @@
-console.log("Funcionou")
+let nota1 = 7;
+let nota2 = 7;
+let resultado = (nota1 + nota2) / 2;
+
+if(resultado >= 7){
+    console.log("Você foi aprovado!")
+} else{
+    console.log("Voce foi reprovado!")
+};
