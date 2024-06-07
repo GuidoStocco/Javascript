46189f0b8ef84f38ad3b52f78fa0ea769369dbf9
Guido Stocco <guidostoccodev@gmail.com>
2024-06-06 21:26:15 -0300

    usando métodos de array

--- 21_Lógica_Algoritmo/js/script.js
+++ 21_Lógica_Algoritmo/js/script.js
@@ -17,3 +17,13 @@ function jurosFinal(valor, taxa, total){
 };
 
 jurosFinal(2000, 0.03, 12);
+
+
+// Exercícios
+// usando o splice
+const cargos = ["fullstack", "front-end", 'backend', 'mobile']
+
+const cargoRemovido = cargos.splice(2,1)
+
+console.log(cargoRemovido);
+console.log(cargos)
