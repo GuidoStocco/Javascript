ba290acf84534d4f25eec26bab1231c944436815
Guido Stocco <guidostoccodev@gmail.com>
2024-06-10 22:14:45 -0300

    desafio 2

--- 21_Lógica_Algoritmo/js/desafios.js
+++ 21_Lógica_Algoritmo/js/desafios.js
@@ -210,3 +210,14 @@ let nomeParaPesquisar = "Matheus";
 
 // Pesquisando telefones pelo nome
 pesquisarTelefonePorNome(nomeParaPesquisar);
+
+
+
+//
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+function valorpositivo(num1, num2, num3) {
+    return num1 > 0 && num2 > 0 && num3 > 0;
+}
