6fa5d73237a814f7cd53e6a007b25bd69cf7a8ab
Guido Stocco <guidostoccodev@gmail.com>
2024-06-17 18:58:10 -0300

    teste

--- 2_exercícios/js/script.js
+++ 2_exercícios/js/script.js
@@ -1,118 +1,119 @@
-// EXERCÍCIOS DE ARRAY E OBJETOS
-const arrayNumeros = [1,2,3,4,5];
+// // EXERCÍCIOS DE ARRAY E OBJETOS
+// const arrayNumeros = [1,2,3,4,5];
 
-console.log(arrayNumeros[0]);
-console.log(arrayNumeros[2]);
-console.log(arrayNumeros[3]);
-
-
-
-//
-const nome = ["Guido", 24];
-
-const profissao = ["Desenvolvedor", "Prado", true, 24];
-
-console.log(nome.length);
-console.log(profissao.length);
+// console.log(arrayNumeros[0]);
+// console.log(arrayNumeros[2]);
+// console.log(arrayNumeros[3]);
 
 
 
-//
-let onibus = {
-    rodas: 8,
-    passageiros: 40,
-    portas: 2
-};
+// //
+// const nome = ["Guido", 24];
 
-console.log(onibus.rodas);
-console.log(onibus.passageiros);
-console.log(onibus.portas);
+// const profissao = ["Desenvolvedor", "Prado", true, 24];
 
-// Adicionando propriedades no onibus
-onibus.janelas = 20;
-delete onibus.rodas;
-console.log(onibus);
-console.log(onibus.janelas);
+// console.log(nome.length);
+// console.log(profissao.length);
 
 
 
-//
-const nomes = ["Guido", "ravena", "bel", "katara", "zoe"];
+// //
+// let onibus = {
+//     rodas: 8,
+//     passageiros: 40,
+//     portas: 2
+// };
 
-if(nomes.includes("Guido")){
-    console.log(`Tem o nome ${nomes[0]}`)
-} else {
-    console.log("O nome não foi encontrado")
-};
+// console.log(onibus.rodas);
+// console.log(onibus.passageiros);
+// console.log(onibus.portas);
 
-
-
-//
-const arr = [1,2,3,4,5,6];
-const arr2 = [1,2,3];
-
-function verificarNumeros(numb) {
-    if(numb.length >= 5){
-        console.log("Muitos elementos")
-    } else {
-        console.log("poucos elementos")
-    };
-};  
-
-verificarNumeros(arr);
-verificarNumeros(arr2);
+// // Adicionando propriedades no onibus
+// onibus.janelas = 20;
+// delete onibus.rodas;
+// console.log(onibus);
+// console.log(onibus.janelas);
 
 
 
-//
-const arrayLoop = [0,5,10,15,20];
+// //
+// const nomes = ["Guido", "ravena", "bel", "katara", "zoe"];
 
-for(let i = 0; i < arrayLoop.length; i++){
-    console.log(arrayLoop[i])
-};
-
-
-
-//
-const jason = {
-    "nome": "Guido",
-    "idade": 24,
-    "profissao": "desenvolvedor"
-};
-
-console.log(jason.profissao)
-console.log(jason.nome)
-console.log(jason.idade);
-
-
-//
-const frase = "vou ser um desenvolvedor fullstack";
-
-fraseCompleta = frase.split(" ");
-
-for(let i = 0; i < fraseCompleta.length; i = i + 1){
-    console.log(fraseCompleta[i])
-};
+// if(nomes.includes("Guido")){
+//     console.log(`Tem o nome ${nomes[0]}`)
+// } else {
+//     console.log("O nome não foi encontrado")
+// };
 
 
 
-//calculadora
-const calculadora = {
-    somar: function(a,b){
-        return a + b
-    },
-    subtrair: function(a,b){
-        return a - b
-    },
-    multiplicar: function(a,b){
-        return a * b
-    },
-    dividir: function(a,b){
-        return a / b
-    }
-};
+// //
+// const arr = [1,2,3,4,5,6];
+// const arr2 = [1,2,3];
 
-console.log(calculadora.somar(10,5));
-console.log(calculadora.subtrair(5,5));
-console.log(calculadora.multiplicar(2,2));
-console.log(calculadora.dividir(10,5));
+// function verificarNumeros(numb) {
+//     if(numb.length >= 5){
+//         console.log("Muitos elementos")
+//     } else {
+//         console.log("poucos elementos")
+//     };
+// };  
+
+// verificarNumeros(arr);
+// verificarNumeros(arr2);
+
+
+
+// //
+// const arrayLoop = [0,5,10,15,20];
+
+// for(let i = 0; i < arrayLoop.length; i++){
+//     console.log(arrayLoop[i])
+// };
+
+
+
+// //
+// const jason = {
+//     "nome": "Guido",
+//     "idade": 24,
+//     "profissao": "desenvolvedor"
+// };
+
+// console.log(jason.profissao)
+// console.log(jason.nome)
+// console.log(jason.idade);
+
+
+// //
+// const frase = "vou ser um desenvolvedor fullstack";
+
+// fraseCompleta = frase.split(" ");
+
+// for(let i = 0; i < fraseCompleta.length; i = i + 1){
+//     console.log(fraseCompleta[i])
+// };
+
+
+
+// //calculadora
+// const calculadora = {
+//     somar: function(a,b){
+//         return a + b
+//     },
+//     subtrair: function(a,b){
+//         return a - b
+//     },
+//     multiplicar: function(a,b){
+//         return a * b
+//     },
+//     dividir: function(a,b){
+//         return a / b
+//     }
+// };
+
+// console.log(calculadora.somar(10,5));
+// console.log(calculadora.subtrair(5,5));
+// console.log(calculadora.multiplicar(2,2));
+// console.log(calculadora.dividir(10,5));
+
