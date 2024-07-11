faddc8ec9bf69d600d813653b7c368d1ae53a811
Guido Stocco <guidostoccodev@gmail.com>
2024-07-11 20:15:43 -0300

    desafio dia da semana

--- 22_curso _jscript/js/script.js
+++ 22_curso _jscript/js/script.js
@@ -1,1 +1,6 @@
 //ARRAYS
+const diaSemana = ['domingo',"segunda", "terça", 'quarta', 'quinta', 'sexta', 'sabado']
+
+const data = new Date();
+const diaSemanaHoje = data.getDay();
+console.log(`Hoje é dia ${diaSemana[diaSemanaHoje]}`);
