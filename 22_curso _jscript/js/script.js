8a632f5542b5bbe9aa408ca102c33100237f8adf
Guido Stocco <guidostoccodev@gmail.com>
2024-07-12 19:38:23 -0300

    criando tabela

--- 22_curso _jscript/js/script.js
+++ 22_curso _jscript/js/script.js
@@ -1,6 +1,8 @@
-//ARRAYS
-const diaSemana = ['domingo',"segunda", "terça", 'quarta', 'quinta', 'sexta', 'sabado']
+// ARRAYS
+// const diaSemana = ['domingo',"segunda", "terça", 'quarta', 'quinta', 'sexta', 'sabado']
+// 
+// const data = new Date();
+// const diaSemanaHoje = data.getDay();
+// console.log(`Hoje é dia ${diaSemana[diaSemanaHoje]}`);
 
-const data = new Date();
-const diaSemanaHoje = data.getDay();
-console.log(`Hoje é dia ${diaSemana[diaSemanaHoje]}`);
+//
