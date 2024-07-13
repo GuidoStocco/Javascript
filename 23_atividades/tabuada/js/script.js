233f68e2a7cbb52ac4bbecc454be40a429822310
Guido Stocco <guidostoccodev@gmail.com>
2024-07-13 20:36:19 -0300

    tabuada

---
+++ 23_atividades/tabuada/js/script.js
@@ -0,0 +1,24 @@
+//selecionar elemento
+const numberInput = document.getElementById("number");
+
+const apagar = document.getElementById("apagar");
+const tabuada = document.querySelector("#tabuada");
+
+
+let tabuadaHtml = '';
+
+//function
+function calcular() {
+    const inputValue = numberInput.value;
+
+    for(let i = 1; i <= 10; i++){
+        tabuadaHtml += `${i} x ${inputValue} = ${i * inputValue} <br>`
+    }
+    
+    tabuada.innerHTML = tabuadaHtml
+
+    
+}
+
+calcular();
+
